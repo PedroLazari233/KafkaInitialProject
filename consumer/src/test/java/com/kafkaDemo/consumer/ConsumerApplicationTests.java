a06fe56f270b849fcb077bcec6098c468a9d07ae
Pedro Lazari <pedro.machado.lazari@ericsson.com>
2022-02-25 10:23:36 -0300

    Refactoring Name of packages: producer/src/test/java/com.producer.producer -> producer/src/test/java/com.kafkaDemo.producer and consumer/src/test/java/com.consumer.consumer -> consumer/src/test/java/com/kafkaDemo.consumer

--- consumer/src/test/java/com/kafkaDemo/consumer/ConsumerApplicationTests.java
+++ consumer/src/test/java/com/kafkaDemo/consumer/ConsumerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.producer.producer;
+package com.kafkaDemo.consumer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProducerApplicationTests {
+class ConsumerApplicationTests {
 
 	@Test
 	void contextLoads() {
